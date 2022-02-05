1339d4a0c67b84385a58c25204cfb3af517d6fd5
LordBombardir <60620262+LordBombardir@users.noreply.github.com>
2022-02-05 12:12:30 +0300

    Поддержка PMMP-4.x

--- DGuard/src/qpi/guard/utils/Events.php
+++ DGuard/src/qpi/guard/utils/Events.php
@@ -10,7 +10,8 @@ use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\Player;
+use pocketmine\player\Player;
+use pocketmine\Server;
 use qpi\guard\DGuard;
 
 class Events implements Listener{
@@ -19,16 +20,16 @@ class Events implements Listener{
         $p = $e->getPlayer();
         $n = $p->getName();
         $block = $e->getBlock();
-        $region = Methods::getInstance()->getRegion($block->getX(), $block->getZ(), $p->getLevel()->getName());
+        $region = Methods::getInstance()->getRegion($block->getPosition()->getX(), $block->getPosition()->getZ(), $p->getWorld()->getDisplayName());
 
         if($region !== ""){
-            if(!$p->isOp()){
+            if(!Server::getInstance()->isOp($p->getName())){
                 $role = Methods::getInstance()->getRole($n, $region);
                 if($role === 0){
-                    $e->setCancelled(true);
+                    $e->cancel();
                     $p->sendTip("§c§lУ вас нет доступа к этой территории§r§f");
                 }else if($role === 1){
-                    $e->setCancelled(true);
+                    $e->cancel();
                     $p->sendTip("§c§lВам не разрешено здесь строить§r§f");
                 }
             }
@@ -39,16 +40,16 @@ class Events implements Listener{
         $p = $e->getPlayer();
         $n = $p->getName();
         $block = $e->getBlock();
-        $region = Methods::getInstance()->getRegion($block->getX(), $block->getZ(), $p->getLevel()->getName());
+        $region = Methods::getInstance()->getRegion($block->getPosition()->getX(), $block->getPosition()->getZ(), $p->getWorld()->getDisplayName());
 
         if($region !== ""){
-            if(!$p->isOp()){
+            if(!Server::getInstance()->isOp($p->getName())){
                 $role = Methods::getInstance()->getRole($n, $region);
                 if($role === 0){
-                    $e->setCancelled(true);
+                    $e->cancel();
                     $p->sendTip("§c§lУ вас нет доступа к этой территории§r§f");
                 }else if($role === 1){
-                    $e->setCancelled(true);
+                    $e->cancel();
                     $p->sendTip("§c§lВам не разрешено здесь строить§r§f");
                 }
             }
@@ -67,44 +68,44 @@ class Events implements Listener{
 
         if(($itemHand == 280 or $itemHand == 271) and PlayerInteractEvent::RIGHT_CLICK_BLOCK == $e->getAction()) {
             if($itemHand == 280){
-                $region = Methods::getInstance()->getRegion($block->getX(), $block->getZ(), $p->getLevel()->getName());
+                $region = Methods::getInstance()->getRegion($block->getPosition()->getX(), $block->getPosition()->getZ(), $p->getWorld()->getDisplayName());
 
                 if($region !== ""){
                     Forms::getInstance()->f_regions_info($p, $region);
                 }else $p->sendMessage("§c§l>§f  В данном месте нет регионов.§r");
 
-                $e->setCancelled(true);
+                $e->cancel();
             }else if($itemHand == 271){
                 $n = strtolower($n);
                 if(isset(DGuard::getInstance()->wand[$n])){
                     if(DGuard::getInstance()->wand[$n]) {
-                        DGuard::getInstance()->set_pos(false, $block->getX(), $block->getZ(), $p->getLevel()->getName(), $p);
-
+                        DGuard::getInstance()->set_pos(true, $block->getPosition()->getX(), $block->getPosition()->getZ(), $p->getWorld()->getDisplayName(), $p);
+                        //printf('1 точка');
                     }else{
-                        DGuard::getInstance()->set_pos(true, $block->getX(), $block->getZ(), $p->getLevel()->getName(), $p);
+                        DGuard::getInstance()->set_pos(false, $block->getPosition()->getX(), $block->getPosition()->getZ(), $p->getWorld()->getDisplayName(), $p);
                     }
                 }else{
-                    DGuard::getInstance()->set_pos(true, $block->getX(), $block->getZ(), $p->getLevel()->getName(), $p);
+                    DGuard::getInstance()->set_pos(true, $block->getPosition()->getX(), $block->getPosition()->getZ(), $p->getWorld()->getDisplayName(), $p);
                 }
-                $e->setCancelled(true);
+                $e->cancel();
 
             }
-        }else if((in_array($id, $blocked_blocks) or in_array($itemHand, $blocked_items)) and !$p->isOp()){
-            $region = Methods::getInstance()->getRegion($block->getX(), $block->getZ(), $p->getLevel()->getName());
+        }else if((in_array($id, $blocked_blocks) or in_array($itemHand, $blocked_items)) and !Server::getInstance()->isOp($p->getName())){
+            $region = Methods::getInstance()->getRegion($block->getPosition()->getX(), $block->getPosition()->getZ(), $p->getWorld()->getDisplayName());
             if($region !== "") {
                 $role = Methods::getInstance()->getRole($n, $region);
                 if($role === 0){
                     if(in_array($id, [64, 193, 195, 196, 197, 194, 71, 96, 167, 107, 183, 184, 185, 187, 186])){
-                        $e->setCancelled(true);
+                        $e->cancel();
                     }else if($id == 54){
-                        if(Methods::getInstance()->getFlag($region, 'chest') == 'deny') $e->setCancelled(true);
+                        if(Methods::getInstance()->getFlag($region, 'chest') == 'deny') $e->cancel();
                     }else if($id == 61 or $id == 62){
-                        if(Methods::getInstance()->getFlag($region, 'furnace') == 'deny') $e->setCancelled(true);
+                        if(Methods::getInstance()->getFlag($region, 'furnace') == 'deny') $e->cancel();
                     }
                 }
 
                 if(in_array($itemHand, $blocked_items)){
-                    if($role < 2) $e->setCancelled(true);
+                    if($role < 2) $e->cancel();
                 }
 
             }
@@ -116,17 +117,17 @@ class Events implements Listener{
             $p = $e->getEntity();
             if($p instanceof Player){
 
-                $region = Methods::getInstance()->getRegion($p->getX(), $p->getZ(), $p->getLevel()->getName());
+                $region = Methods::getInstance()->getRegion($p->getPosition()->getX(), $p->getPosition()->getZ(), $p->getWorld()->getDisplayName());
                 if($region !== ""){
                     if($e->getDamager() instanceof Player)
-                        if(Methods::getInstance()->getFlag($region, 'pvp') == 'deny') $e->setCancelled(true);
+                        if(Methods::getInstance()->getFlag($region, 'pvp') == 'deny') $e->cancel();
                 }
             }else{
                 $p = $e->getEntity();
 
-                $region = Methods::getInstance()->getRegion($p->getX(), $p->getZ(), $p->getLevel()->getName());
+                $region = Methods::getInstance()->getRegion($p->getPosition()->getX(), $p->getPosition()->getZ(), $p->getWorld()->getDisplayName());
                 if($region !== ""){
-                    if(Methods::getInstance()->getFlag($region, 'pve') == 'deny') $e->setCancelled(true);
+                    if(Methods::getInstance()->getFlag($region, 'pve') == 'deny') $e->cancel();
                 }
             }
         }
