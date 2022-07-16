b075fc062207383d71c23c0925769b91bea334b0
qPexLegendary <terentev.andrey.2002@gmail.com>
2022-07-16 17:02:50 +0700

    Исправлена ошибка добавления мира в список регионов

--- DGuard/src/qpi/guard/event/listener/RegionManagementListener.php
+++ DGuard/src/qpi/guard/event/listener/RegionManagementListener.php
@@ -5,6 +5,7 @@ namespace qpi\guard\event\listener;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\world\WorldInitEvent;
+use pocketmine\event\world\WorldLoadEvent;
 use qpi\guard\region\RegionsList;
 
 class RegionManagementListener implements Listener {
@@ -13,7 +14,11 @@ class RegionManagementListener implements Listener {
 
     }
 
-    public function onWorldInit(WorldInitEvent $event){
+    public function onWorldInit(WorldInitEvent $event): void {
+        $this->regions->initWorld($event->getWorld());
+    }
+
+    public function onWorldCreated(WorldLoadEvent $event): void {
         $this->regions->initWorld($event->getWorld());
     }
 
